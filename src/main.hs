e507f460ab52df58e7fcaf3ff3fc15af2c6cf6de
Kai <u7moench@gmail.com>
2020-12-28 20:58:00 +0100

    Work on chess move gen logic

--- src/main.hs
+++ src/main.hs
@@ -27,38 +27,47 @@ movesFromSquare gameState position = case board gameState !! fst position !! snd
   Full p -> makeMoves p position gameState
 
 makeMoves :: Piece -> Sqr -> GameState -> [GameState]
-makeMoves piece from gameState = filter (isLegalMove gameState from) (map (makeMove gameState from) (destinationSquares piece from))
+makeMoves piece from gameState = map (makeMove gameState) (filter (isLegalMove gameState) (destinationSquares piece from))
 
 makeMove :: GameState -> Move -> GameState 
 makeMove gameState move = defaultGameState 
 
-destinationSquares :: Piece -> Sqr -> [Sqr] 
-destinationSquares piece pos = case piece of
-  Piece White Pawn -> pawnMovesUp pos
-  Piece Black Pawn -> pawnMovesDown pos
-  Piece White Knight -> knightMoves pos
-  Piece Black Knight -> knightMoves pos
-  Piece White Bishop -> bishopMoves pos
-  Piece Black Bishop -> bishopMoves pos
-  Piece White Rook -> rookMoves pos
-  Piece Black Rook -> rookMoves pos
-  Piece White Queen -> queenMoves pos
-  Piece Black Queen -> queenMoves pos
-  Piece White King -> kingMoves pos
-  Piece Black King -> kingMoves pos
+destinationSquares :: Piece -> Sqr -> [Move] 
+destinationSquares piece from = case piece of
+  Piece White Pawn -> [ (x, y) | x <- pawnMovesUp from, y <- repeat from]
+  Piece Black Pawn -> [ (x, y) | x <- pawnMovesDown from, y <- repeat from]
+  x -> []
 
-isLegalMove :: GameState -> GameState -> Bool
-isLegalMove a b = False 
---not (isFriendlyFire gs from to) && not (isCollision gs from to) && not (isSuicide gs from to)
+isLegalMove :: GameState -> Move -> Bool
+isLegalMove state move = not (isFriendlyFire (board state) move || isCollision state move || isSuicide state move || isPieceSpecificIllegal state move)
 
---isFriendlyFire :: GameState -> Pos -> Pos -> Bool
---isFriendlyFire gs from to = False
---
---isCollision :: GameState -> Pos -> Pos -> Bool
---isCollision gs from to = False
---
---isSuicide :: GameState -> Pos -> Pos -> Bool
---isSuicide gs from to = False
+at :: Board -> Sqr -> Square 
+at b (x, y) = b !! x !! y
+
+pieceColor :: Board -> Sqr -> Maybe Color 
+pieceColor board pos = case board `at` pos of
+  Empty -> Nothing 
+  Full p -> Just (color p)
+
+-- You can not take your own pieces. 
+isFriendlyFire :: Board -> Move -> Bool
+isFriendlyFire board (to, from) = pieceColor board from == pieceColor board to
+
+-- You can not jump over your own pieces (except knights). 
+isCollision :: Board -> Move -> Bool
+isCollision board (to, from) = 
+  let nextX = x from + signum (x to - x from);
+      nextY = y from + signum (y to - x to) in
+  any [x y | x <- [(x from)..], y <- [(y from)..]] pieceColor 
+
+-- You can make not get your self checked. 
+isSuicide :: GameState -> Move -> Bool
+isSuicide state move = False
+
+-- Pawns can only capture diagonally, Castling rules must be valid, En passant must be valid.
+isPieceSpecificIllegal :: GameState -> Move -> Bool
+isPieceSpecificIllegal state move = False
+
 --
 --outSideBoard (a, b) = (0 > a) || (a > 7) || (0 > b) || (b > 7)
 
@@ -78,6 +87,7 @@ queenMoves (a, b) = rookMoves (a, b) ++ bishopMoves (a, b)
 kingMoves (a, b) = [(a -1, b -1), (a -1, b), (a -1, b + 1), (a, b -1), (a, b + 1), (a + 1, b -1), (a + 1, b), (a + 1, b + 1)]
 
 -- Generally possible Downwards Pawn moves from a position. -- TODO at start 2 squares
+pawnMovesUp :: Sqr  -> [Sqr]
 pawnMovesUp (a, b) = [(a, b + 1), (a - 1, b + 1), (a + 1, b + 1)]
 
 -- Generally possible Upwards Pawn moves from a position. -- TODO at start 2 sqaures 
