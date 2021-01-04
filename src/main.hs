ee4f04b0ac2210a64123bf5eee211613bfa58d92
Kai <u7moench@gmail.com>
2021-01-04 21:54:22 +0100

    some basic min max search

--- src/main.hs
+++ src/main.hs
@@ -7,8 +7,46 @@ import Data.Tree
 -- Should give us a list with the 20 possible positions after first half-move.
 testGenMoves = generateMoves defaultGameState
 
-gameTree = unfoldTree (\p -> ("some label", generateMoves p)) defaultGameState
+makeNode state = (state, generateMoves state) 
+
+gameTree = unfoldTree makeNode defaultGameState
 
 boards = map board testGenMoves
 
-main = putStrLn (foldl (\l r -> concat [l, pprintBoard r, "---\n"]) "" boards)
+prune :: Int -> Tree a -> Tree a
+prune 0 (Node x ts) = Node x []
+prune depth tree = prune (depth - 1) tree 
+
+staticEval :: Board -> Double
+staticEval b = sum (map squareValue (concat b))
+
+squareValue :: Square -> Double
+squareValue s = case s of
+    Empty -> 0
+    Full (Piece White p) -> pieceValue p
+    Full (Piece Black p) -> negate (pieceValue p)
+
+pieceValue :: PType -> Double  
+pieceValue p = case p of
+        Pawn -> 1
+        Knight -> 3
+        Bishop -> 3.3
+        Rook -> 5
+        Queen -> 9
+        King -> 1000
+        
+maximise :: Tree Double -> Double 
+maximise (Node n []) = n 
+maximise (Node n subTree) = maximum (map minimise subTree)
+
+minimise :: Tree Double -> Double 
+minimise (Node n []) = n
+minimise (Node n subTree) = minimum (map maximise subTree)
+
+smallTree = prune 5 gameTree
+
+smallTreeStaticEvaluated = fmap (staticEval .board) smallTree
+
+eval = maximise smallTreeStaticEvaluated
+
+main = print eval
